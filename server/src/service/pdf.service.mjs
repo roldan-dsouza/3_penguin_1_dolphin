8a135a09dde320d26c27676631212904beb7d900
Nidhi Anna Sequeira <sequeiranidhianna@gmail.com>
2026-02-26 04:41:03 +0530

    .

--- server/src/service/pdf.service.mjs
+++ server/src/service/pdf.service.mjs
@@ -9,7 +9,6 @@ const pdfParse = require("pdf-parse");
 const extractTextFromPDF = async (filePath) => {
   const dataBuffer = fs.readFileSync(filePath);
   const data = await pdfParse(dataBuffer);
-
   return data.text;
 };
 
