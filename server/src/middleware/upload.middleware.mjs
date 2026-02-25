f5a4846b99379c1f6baf2759c3737a0b6ad850d0
Nidhi Anna Sequeira <sequeiranidhianna@gmail.com>
2026-02-25 17:28:22 +0530

    feat: add middleware for multer

---
+++ server/src/middleware/upload.middleware.mjs
@@ -0,0 +1,29 @@
+// server/src/middleware/upload.middleware.mjs
+
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage(
+  {
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+  },
+  {
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  },
+);
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype != "application/pdf") {
+      return cb(new Error("Only PDF files allowed"));
+    }
+    cb(null, true);
+  },
+});
+
+export default upload;
