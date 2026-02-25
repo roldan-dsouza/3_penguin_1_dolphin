6c172b64225383803263b182b485374e641825c5
shadow1951 <roldannov@gmail.com>
2026-02-25 18:34:28 +0530

    clean-up:remove import and the endpoint of addBoarding from auth routes

--- server/src/routes/auth.routes.mjs
+++ server/src/routes/auth.routes.mjs
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { addOnboarding, login } from "../controllers/auth.controller.mjs";
+import { login } from "../controllers/auth.controller.mjs";
 const router = Router();
 
 router.post("/login", login);
-router.post("/onboarding", addOnboarding);
 
 export default router;
