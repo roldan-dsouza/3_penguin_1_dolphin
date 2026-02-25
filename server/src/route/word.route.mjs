fffd8076cc57e308f12e15cb79a1b9ef509be013
shadow1951 <roldannov@gmail.com>
2026-02-25 22:13:00 +0530

    feat:add text simplifer

--- server/src/route/word.route.mjs
+++ server/src/route/word.route.mjs
@@ -2,11 +2,13 @@
 
 import express from "express";
 import devAuthMiddleware from "../middleware/dev.auth.middleware.mjs";
+import { simplifyHtmlForDyslexia } from "../controller/word.controller.mjs";
 
 const router = express.Router();
 
 router.use(devAuthMiddleware);
 
 //router.post("/meaning", getWordMeaning);
+router.get("/enhanced", simplifyHtmlForDyslexia);
 
 export default router;
